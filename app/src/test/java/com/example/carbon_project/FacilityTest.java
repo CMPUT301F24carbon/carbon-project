67050c238e182f0e4de7df323140e8809802de37
gcheema7 <gcheema7@hotmail.com>
2024-11-07 22:43:16 -0700

    added event tests

--- app/src/test/java/com/example/carbon_project/FacilityTest.java
+++ app/src/test/java/com/example/carbon_project/FacilityTest.java
@@ -4,8 +4,6 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-import com.example.carbon_project.Facility;
-
 public class FacilityTest {
 
     private Facility facility;
