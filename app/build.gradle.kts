b7dd5f259fd6f0f46e029081050e3c8cb4811ef0
Abdulrehman Zia <arzia164@gmail.com>
2024-11-29 15:59:14 -0700

    Added firebase-functions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.storage)
     implementation(libs.firebase.messaging)
+    implementation(libs.firebase.functions)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
