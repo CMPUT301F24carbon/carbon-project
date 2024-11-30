0a3d077b08cc150476e6351ec77e01ebf2d8de91
Abdulrehman Zia <arzia164@gmail.com>
2024-11-29 18:09:53 -0700

    The changes necessary to recieve notifications

--- app/src/main/java/com/example/carbon_project/Controller/MainActivity.java
+++ app/src/main/java/com/example/carbon_project/Controller/MainActivity.java
@@ -58,17 +58,28 @@ public class MainActivity extends AppCompatActivity {
 }*/
 package com.example.carbon_project.Controller;
 
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.carbon_project.Model.Admin;
 import com.example.carbon_project.Model.Entrant;
+import com.example.carbon_project.Model.Notification;
 import com.example.carbon_project.Model.Organizer;
 import com.example.carbon_project.Model.User;
 import com.example.carbon_project.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /*
 
@@ -211,24 +222,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //this checks and asks for permissions
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                // Ask for permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.POST_NOTIFICATIONS},
+                        1);
+            }
+        }
+
+
+        //this allows the app to recieve notifications in the background
+        NotificationChannel channel = new NotificationChannel(
+                "eventChannel",
+                "Event Notifications",
+                NotificationManager.IMPORTANCE_HIGH);
+        NotificationManager manager = getSystemService(NotificationManager.class);
+        if (manager != null) {
+            manager.createNotificationChannel(channel);
+        }
+
         String deviceId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
 
-        User gagan = new User(deviceId, "Yingwen Liu", "pleasepass@example.com", "123-456-789", "Organizer", true);
-
-        findViewById(R.id.entrant_button).setOnClickListener(view -> createUser(gagan, Entrant.class));
-        findViewById(R.id.organizer_button).setOnClickListener(view -> createUser(gagan, Organizer.class));
-        findViewById(R.id.admin_button).setOnClickListener(view -> createUser(gagan, Admin.class));
+        User abdul = new User(deviceId, "Abdulrehman Zia", "pleasepass@example.com", "123-456-789", "Organizer");
+        findViewById(R.id.entrant_button).setOnClickListener(view -> createUser(abdul, Entrant.class));
+        findViewById(R.id.organizer_button).setOnClickListener(view -> createUser(abdul, Organizer.class));
+        findViewById(R.id.admin_button).setOnClickListener(view -> createUser(abdul, Admin.class));
     }
 
     private void createUser(User user, Class<? extends User> userType) {
         User userInstance;
 
         if (userType == Entrant.class) {
-            userInstance = new Entrant(user.getUserId(), user.getName(), user.getEmail(), user.getPhoneNumber(), user.getNotifications());
+            userInstance = new Entrant(user.getUserId(), user.getName(), user.getEmail(), user.getPhoneNumber());
         } else if (userType == Organizer.class) {
-            userInstance = new Organizer(user.getUserId(), user.getName(), user.getEmail(), user.getPhoneNumber(), user.getNotifications());
+            userInstance = new Organizer(user.getUserId(), user.getName(), user.getEmail(), user.getPhoneNumber());
         } else if (userType == Admin.class) {
-            userInstance = new Admin(user.getUserId(), user.getName(), user.getEmail(), user.getPhoneNumber(), user.getNotifications());
+            userInstance = new Admin(user.getUserId(), user.getName(), user.getEmail(), user.getPhoneNumber());
         } else {
             throw new IllegalArgumentException("Unknown user type");
         }
