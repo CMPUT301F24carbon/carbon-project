b691108ad4eb12f979237cea49000d12cc689b3b
Lilacrity <ayyub@ualberta.ca>
2024-11-30 20:16:52 -0700

    added admin profile viewing

--- app/src/main/java/com/example/carbon_project/Controller/AdminUserListFragment.java
+++ app/src/main/java/com/example/carbon_project/Controller/AdminUserListFragment.java
@@ -1,5 +1,6 @@
 package com.example.carbon_project.Controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.example.carbon_project.Model.Entrant;
 import com.example.carbon_project.Model.Facility;
 import com.example.carbon_project.Model.User;
 import com.example.carbon_project.R;
@@ -50,7 +52,9 @@ public class AdminUserListFragment extends Fragment{
 
         AdminRecyclerViewAdapter adapter = new AdminRecyclerViewAdapter(data, position -> {
             User clickedUser = (User) data.get(position);
-            Toast.makeText(getContext(), "Clicked " + clickedUser.getName(), Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(requireContext(), AdminViewProfileActivity.class);
+            intent.putExtra("userObject", clickedUser);
+            startActivity(intent);
         });
         recyclerView.setAdapter(adapter);
 
