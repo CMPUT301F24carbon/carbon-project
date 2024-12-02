af31b3eaf22a4b1a083b4f1919cc2975642e34d6
Sarthak-AgarwalGit <144981786+Sarthak-AgarwalGit@users.noreply.github.com>
2024-12-01 22:43:42 -0700

    Update Organizer.java

--- app/src/main/java/com/example/carbon_project/Model/Organizer.java
+++ app/src/main/java/com/example/carbon_project/Model/Organizer.java
@@ -7,34 +7,109 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * The Organizer class represents a user who is an event organizer in the system.
+ * It extends the User class and provides additional methods for managing created events
+ * and associated facilities. Organizers can add or remove events, manage facilities, 
+ * and store their data in Firestore.
+ */
 public class Organizer extends User implements Serializable {
-    private List<String> createdEvents;
-    private List<String> facilityIds;
+    
+    private List<String> createdEvents;  // List of event IDs created by the organizer
+    private List<String> facilityIds;    // List of facility IDs associated with the organizer
 
+    /**
+     * Constructor for creating a new Organizer instance.
+     *
+     * @param userId      The unique user ID of the organizer.
+     * @param name        The name of the organizer.
+     * @param email       The email address of the organizer.
+     * @param phoneNumber The phone number of the organizer.
+     */
     public Organizer(String userId, String name, String email, String phoneNumber) {
         super(userId, name, email, phoneNumber, "organizer");
         this.createdEvents = new ArrayList<>();
         this.facilityIds = new ArrayList<>();
     }
 
-    // Specific methods for organizers
-    public void addEvent(String eventId) { createdEvents.add(eventId); }
-    public void removeEvent(String eventId) { createdEvents.remove(eventId); }
-    public List<String> getCreatedEvents() { return createdEvents; }
+    // Specific methods for managing events
+
+    /**
+     * Adds an event to the list of events created by the organizer.
+     *
+     * @param eventId The ID of the event to be added.
+     */
+    public void addEvent(String eventId) {
+        createdEvents.add(eventId);
+    }
+
+    /**
+     * Removes an event from the list of events created by the organizer.
+     *
+     * @param eventId The ID of the event to be removed.
+     */
+    public void removeEvent(String eventId) {
+        createdEvents.remove(eventId);
+    }
+
+    /**
+     * Retrieves the list of event IDs created by the organizer.
+     *
+     * @return A list of event IDs.
+     */
+    public List<String> getCreatedEvents() {
+        return createdEvents;
+    }
 
     // Methods for managing facilities
-    public void addFacility(String facilityId) { facilityIds.add(facilityId); }
-    public void removeFacility(String facilityId) { facilityIds.remove(facilityId); }
-    public List<String> getFacilityIds() { return facilityIds; }
 
-    // Convert Organizer object to a Map for Firestore storage
+    /**
+     * Adds a facility to the list of facilities associated with the organizer.
+     *
+     * @param facilityId The ID of the facility to be added.
+     */
+    public void addFacility(String facilityId) {
+        facilityIds.add(facilityId);
+    }
+
+    /**
+     * Removes a facility from the list of facilities associated with the organizer.
+     *
+     * @param facilityId The ID of the facility to be removed.
+     */
+    public void removeFacility(String facilityId) {
+        facilityIds.remove(facilityId);
+    }
+
+    /**
+     * Retrieves the list of facility IDs associated with the organizer.
+     *
+     * @return A list of facility IDs.
+     */
+    public List<String> getFacilityIds() {
+        return facilityIds;
+    }
+
+    /**
+     * Converts the Organizer object to a Map for Firestore storage.
+     * This includes the base properties of the User class as well as the
+     * specific properties of the Organizer (i.e., created events).
+     *
+     * @return A map containing the Organizer's data for Firestore storage.
+     */
+    @Override
     public Map<String, Object> toMap() {
         Map<String, Object> map = super.toMap();
         map.put("createdEvents", createdEvents);
+        map.put("facilityIds", facilityIds);
         return map;
     }
 
-    // Save the Organizer to Firestore
+    /**
+     * Saves the Organizer's data to Firestore under the "users" collection.
+     * This method stores the Organizer object by converting it to a Map and saving it
+     * using the user ID as the document identifier.
+     */
     public void saveToFirestore() {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         db.collection("users")
@@ -47,5 +122,4 @@ public class Organizer extends User implements Serializable {
                     System.out.println("Error saving organizer: " + e.getMessage());
                 });
     }
-
 }
